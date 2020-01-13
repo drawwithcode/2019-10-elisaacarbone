aa33a210d54b658b2b545935f5978fb82f89555c
Elisa Carbone <carbone.elisa.98.ec@gmail.com>
2020-01-13 15:30:38 +0100

    aa

--- sketch.js
+++ sketch.js
@@ -33,33 +33,37 @@ function draw() {
 //create the ambient light
   ambientLight(50, 50, 100);
 
+//create a directional light that moves with the mouse
   var dirX = (mouseX / width - 0.5) * 2;
   var dirY = (mouseY / height - 0.5) * 2;
   directionalLight(255, 255, 255, dirX, dirY, -0.1);
 
-
+//define the appearence of every 3D shape
   noStroke();
+
+//allign the system with the center of the page
   translate(radius/4, -radius/4);
 
   push();
   fill(255);
-  torus(107, 0.2);
-  torus(125, 0.2);
-  torus(145, 0.2);
-  torus(165, 0.2);
-  torus(190, 0.2);
-  torus(225, 0.2);
-  torus(245, 0.2);
-  torus(265, 0.2);
+  torus(107, 0.2); //orbit mercury
+  torus(125, 0.2); //orbit venus
+  torus(145, 0.2); //orbit earth
+  torus(165, 0.2); //orbit mars
+  torus(190, 0.2); //orbit jupiter
+  torus(225, 0.2); //orbit saturn
+  torus(245, 0.2); //orbit uranus
+  torus(265, 0.2); //orbit neptune
   pop();
 
-
+//create the sun
   push();
   rotateY(frameCount * 0.005);
   texture(sun);
   sphere(radius);
   pop();
 
+//create mercury
   rotateZ(frameCount * 0.0025);
   push();
   texture(mercury);
@@ -67,6 +71,7 @@ function draw() {
   sphere(2);
   pop();
 
+//create venus
   rotateZ(frameCount * 0.003);
   push();
   texture(venus);
@@ -74,6 +79,7 @@ function draw() {
   sphere(4);
   pop();
 
+//create earth
   rotateZ(frameCount * 0.004);
   push();
   texture(earth);
@@ -81,6 +87,7 @@ function draw() {
   sphere(4);
   pop();
 
+//create mars
   rotateZ(frameCount * 0.005);
   push();
   texture(mars);
@@ -88,6 +95,7 @@ function draw() {
   sphere(3.5);
   pop();
 
+//create jupiter
   rotateZ(frameCount * 0.0055);
   push();
   texture(jupiter);
@@ -95,6 +103,7 @@ function draw() {
   sphere(15);
   pop();
 
+//create saturn
   rotateZ(frameCount * 0.0057);
   push();
   texture(saturn);
@@ -102,6 +111,7 @@ function draw() {
   sphere(13);
   pop();
 
+//create uranus
   rotateZ(frameCount * 0.0059);
   push();
   texture(uranus);
@@ -109,6 +119,7 @@ function draw() {
   sphere(6);
   pop();
 
+//create neptune
   rotateZ(frameCount * 0.006);
   push();
   texture(neptune);
@@ -117,6 +128,7 @@ function draw() {
   pop();
 }
 
+//let the window respond to resizing
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
 }
